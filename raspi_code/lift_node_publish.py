cb3690926946527f7e26f7016d6a179126f8290f
samuel ang <samuelangdj@gmail.com>
2021-08-24 22:18:07 +0800

    added some stuff

--- raspi_code/lift_node_publish.py
+++ raspi_code/lift_node_publish.py
@@ -10,14 +10,14 @@ def on_publish(client,userdata,result):             #create function for callbac
     print("data published ", time.time())
 
 def main():
-    client = mqtt.Client("lift_pub")  # Create instance of client with client ID “lift”
+    client = mqtt.Client("lift1_pub")  # Create instance of client with client ID “lift”
     client.on_connect = on_connect  # Define callback function for successful connection
     client.on_publish = on_publish   
     client.connect('fc94:cec3:39f9:6e9e:ec86:a600:ead4:d41a', 1883)  # husarnet ipv6 address
     client.loop_start()  # Start networking daemon
     while True:
         #include lift msg
-        client.publish("lift/lift_states", ??)
+        client.publish("lift/lift1/lift_states", ??)
         time.sleep(1)
 
 if name == 'main':
